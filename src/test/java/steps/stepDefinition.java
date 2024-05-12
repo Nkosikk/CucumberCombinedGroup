f1b6fb9201bfe98b6889b164fad396f51b1bc971
skho.ntuli <skho27@gmail.com>
2024-05-12 17:46:16 +0200

    Added feature files and step definition

--- src/test/java/steps/stepDefinition.java
+++ src/test/java/steps/stepDefinition.java
@@ -1,35 +1,41 @@
+package steps;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class stepDefination {
-
+public class stepDefinition {
+    WebDriver driver;
     @Given("I am on Login page")
     public void i_am_on_login_page() {
-        WebDriver driver;
+
         driver=new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
-
+        driver.manage().window().maximize();
+//        driver.get(url);
+//        return driver;
     }
-    @And("^I enter the username (.*)$")
+    @And("I enter the username {}")
     public void i_enter_the_username_user_name(String username) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
 
     }
-    @And("^I enter password (.*)$")
+    @And("I enter password {}")
     public void i_enter_password_pass(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
 
     }
     @When("I click login button")
     public void i_click_login_button() {
+     driver.findElement(By.id("login-button")).click();
 
     }
     @Then("The home page is displayed")
     public void the_home_page_is_displayed() {
 
     }
-
-
 }
