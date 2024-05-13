7c4affdc5e2805bddfda4a119f6aa44abcb2aa89
Cele <njceles@gmail.com>
2024-05-13 19:04:49 +0200

    completed the basic project

--- src/test/java/steps/TestRunner.java
+++ src/test/java/steps/TestRunner.java
@@ -1,5 +1,12 @@
 package steps;
 
-public class TestRunner {
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features",
+        glue = {"steps"},
+        plugin = {"pretty", "html:target/sauce Demo Report.html"})
+public class TestRunner {
 }
